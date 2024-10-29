3b693a20ed80a4f1236eae748b16ae3c33160dd3
LucasRS64 <121622742+LucasRS64@users.noreply.github.com>
2024-10-29 09:36:55 -0300

    Add files via upload

---
+++ Exercicios C#/PredioXYZ/Exe_Sec5/ConsoleApp1/ConsoleApp1/ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace Exercicio {
+    class ContaBancaria {
+
+        private string _numero;
+        public string Nome_Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria() {
+
+        }
+
+        public ContaBancaria(string numero, string nome_titular) : this() {
+            Numero = numero;
+            Nome_Titular = nome_titular;
+            Saldo = 0;
+        }
+
+        public ContaBancaria(string numero, string nome_titular, double saldo) : this(numero, nome_titular) {
+            Saldo = saldo;
+
+        }
+        public string Numero {
+            get { return _numero; }
+            set {
+                if (value != null && value.Length > 4) {
+                    _numero = value;
+                }
+            }
+        }
+        public void Saque(double valorSaque) {
+         
+         Saldo = (Saldo - valorSaque) - 5.00;
+         
+          
+        }
+
+        public void Deposito(double valorDeposito) {
+            if (valorDeposito > 0) {
+                Saldo += valorDeposito;
+            }
+            else {
+                Console.WriteLine("Valor de depósito inválido.");
+            }
+
+        }
+
+        public override string ToString() {
+            return "Conta " + _numero + " Titular: "
+                    + Nome_Titular + " Saldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture); 
+        }
+
+    }
+}
