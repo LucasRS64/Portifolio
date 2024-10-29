0cff4bdaffe4148f3b6e8f8ffc0b5705a9f4572b
LucasRS64 <121622742+LucasRS64@users.noreply.github.com>
2024-10-29 09:39:47 -0300

    Add files via upload

---
+++ Exercicios C#/PredioXYZ/ExercicioStatic/ExercicioStatic/ConversorDeMoeda.cs
@@ -0,0 +1,13 @@
+﻿namespace ExercicioStatic {
+    class ConversorDeMoeda {
+
+        public static double IOF = 0.06;
+
+        public static double ConversaoReal(double valor) { 
+            return valor + valor * IOF;
+        }
+        public static double Conversao(double val, double x) {
+            return val * x;
+        }
+    }
+}
